252cb2275a4fedcc0feb26a5ff6912b224f0fc31
illyakurochkin <illya.kurochkin@gmail.com>
2020-01-31 18:39:11 +0200

    Second commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,37 @@
-const {src, dest} = require('gulp');
-const babel = require('gulp-babel');
+const gulp = require('gulp');
+const cssnano = require('gulp-cssnano');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
-exports.default = function defaultTask() {
-  console.log('src');
-  return src('src/*.js')
-    .pipe(babel())
-    .pipe(src('vendor/*.js'))
+gulp.task('styles', function(){
+  return gulp.src('src/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(concat('index.css'))
+    .pipe(gulp.dest('output'));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src(['src/scripts/**/*.js'])
+    .pipe(babel({presets: ['@babel/env']}))
+    .pipe(concat('index.js'))
     .pipe(uglify())
-    .pipe(dest('output/'));
-};
+    .pipe(gulp.dest('output'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('output/images/'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/styles/**/*.scss', gulp.series('styles'));
+  gulp.watch('src/scripts/**/*.js', gulp.series('scripts'));
+  gulp.watch('src/images/*', gulp.series('images'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'scripts', 'images', 'watch'));
